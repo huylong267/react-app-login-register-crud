c729a9cfe0594b13ab39e5f13e21d7b0157156f9
huylong267 <h.long195@gmail.com>
2020-03-25 23:51:32 +0700

    create project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+import './App.css';
+import Login from './components/login';
+
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+            hello
+            <Login></Login>
+          </div>
+      );
+  }
+}
+
+export default App;
