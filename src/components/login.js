7e6acbb4524785954e5efd042a5a351e5cb8d544
huylong267 <h.long195@gmail.com>
2020-03-27 00:37:10 +0700

    UI inpput login

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,33 @@
 import React,{ Component } from "react";
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             userName :'',
+             password :''
+        }
+    }
+    
     render(){
         return (
-            <div>
-                hello
+            <div className="container">
+                <div className="card card-login mx-auto mt-5">
+                    <div className="card-header">Login</div>
+                    <div className="card-body">
+                        <form action="">
+                            <div className="form-group">
+                                <div className="form-label-group">
+                                    <input type="text" id="inputEmail" placeholder="Email address" name="email" className="form-group"/>
+                                    <label htmlFor="inputEmail">Email address</label>
+                                    <div className="invalid-feedback"></div>     
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
